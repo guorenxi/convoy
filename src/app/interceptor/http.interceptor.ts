8ad05b9379c8cce5c293a7cb147bebd061b070b1
Pelumi Honey <pelumihoney@Pelumis-MacBook-Pro.local>
2021-12-31 03:54:44 +0100

    login flow and forgot password flow

--- src/app/interceptor/http.interceptor.ts
+++ src/app/interceptor/http.interceptor.ts
@@ -3,10 +3,11 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { GeneralService } from '../services/general/general.service';
 
 @Injectable()
 export class HttpIntercepter implements HttpInterceptor {
-	constructor(private router: Router) {}
+	constructor(private router: Router, private generalService: GeneralService) {}
 
 	intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 		return next.handle(request).pipe(
@@ -15,6 +16,16 @@ export class HttpIntercepter implements HttpInterceptor {
 			}),
 			catchError((error: HttpErrorResponse) => {
 				if (error.status === 401) this.router.navigate(['/login'], { replaceUrl: true });
+				let errorMessage: string;
+
+				if (Array.isArray(error.error?.errors) && error.error?.errors.length > 0) {
+					errorMessage = error.error?.errors[0]?.message;
+				} else {
+					errorMessage = error.error?.message;
+				}
+				this.generalService.showNotification({
+					message: `${error.message === 'Timeout has occurred' ? 'Request timeout, please check your network and try again' : errorMessage}`
+				});
 				return throwError(error);
 			})
 		);
---
+++ src/app/public/login/login.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HTTP_RESPONSE } from 'src/app/models/http.model';
+import { HttpService } from 'src/app/services/http/http.service';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class LoginService {
+	constructor(private http: HttpService) {}
+
+	async login(requestDetails: { email?: string; password?: string }): Promise<HTTP_RESPONSE> {
+		try {
+			const response = await this.http.request({
+				url: 'login',
+				body: requestDetails,
+				method: 'post'
+			});
+			return response;
+		} catch (error: any) {
+			return error;
+		}
+	}
+	
+	async logout(): Promise<HTTP_RESPONSE> {
+		try {
+			const response = await this.http.request({
+				url: 'logout',
+				method: 'delete'
+			});
+			return response;
+		} catch (error: any) {
+			return error;
+		}
+	}
+}
